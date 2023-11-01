e54c0a97e238b401a70b91f70a070646e15600e2
MichaelCarrr <michaelscarr02@gmail.com>
2023-10-31 20:08:52 -0400

    Added files Home.js and Home.css to make home page

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import About from "./pages/About";
+import About from "./pages/About.js";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/about" component={About} />
-      </Switch>
-    </Router>
+        <Router>
+          <ul>
+          <li>
+            <Link to={`/`}>-Home</Link>
+            <Link to={`/About`}>  -About</Link>
+          </li>
+          </ul>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </Router>
+  
+
+    
   );
 }
 
---
+++ app/src/pages/Home.js
@@ -0,0 +1,12 @@
+import "./Home.css";
+function Home() {
+  return (
+          <div className="center">
+            <h1>Welcome to TuneTalk!</h1>
+
+            Login or Register below!
+          </div>
+  );
+}
+
+export default Home;
