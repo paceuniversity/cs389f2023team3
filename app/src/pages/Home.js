3946e8124a304b2a2fc5b9f8e296f294265a22fd
MichaelCarrr <michaelscarr02@gmail.com>
2023-11-05 15:33:28 -0500

    Revert "Merge branch 'main' into michaels-branch"
    This reverts commit 46d8496c16ad1a59409a1cfe844ed35d5151da75, reversing
    changes made to f37cf6f9e24f52c5c69f68b5f2f7198c2f11ccc7.

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -3,12 +3,11 @@ import "./Home.css";
 import { Login } from '../components/auth/Login';
 import { Register } from '../components/auth/Register';
 import AuthDetails from '../components/auth/AuthDetails';
-import { Post } from '../pages/Post';
+
 import React, { useState } from "react";
 
 function Home() {
   const [currentForm, setCurrentForm] = useState('login');
-  const isAuthenticated = true;
 
   const toggleForm = (formName) => { 
     setCurrentForm(formName);
@@ -17,7 +16,7 @@ function Home() {
   return (
   <div className="home-page">
    
-    {!isAuthenticated && <div className="login center">
+    <div className="center">
       <h1>Welcome to TuneTalk!</h1>
 
       Login or Register below!
@@ -27,59 +26,10 @@ function Home() {
 
        
       }
-   AuthDetails />
-   </div>
-
-  {isAuthenticated && <div className="posts">
-      {postsArray.map((data, index) => (
-        <Post key={index} postData={data} />
-      ))}
-    </div>}
+      < AuthDetails />
+    </div>
   </div>
   );
 }
-
-const postsArray = [
-  {
-    userName: "Tiffany",
-    date: "November 1, 2023",
-    description: "Check out this album!",
-    artist: "Mac Miller",
-    title: "Live From Space",
-    coverUrl: "https://upload.wikimedia.org/wikipedia/en/5/5f/Mac_Miller_Live_from_Space.jpg"
-  },
-  {
-    userName: "Alex",
-    date: "November 2, 2023",
-    description: "My favorite of all times.",
-    artist: "The Beatles",
-    title: "Abbey Road",
-    coverUrl: "https://upload.wikimedia.org/wikipedia/en/4/42/Beatles_-_Abbey_Road.jpg"
-  },
-  {
-    userName: "Nate",
-    date: "November 3, 2023",
-    description: "Classic rock vibes!",
-    artist: "The Rolling Stones",
-    title: "Sticky Fingers",
-    coverUrl: "https://amateurphotographer.com/wp-content/uploads/sites/7/2021/12/010.jpg"
-  },
-  {
-    userName: "Jason",
-    date: "November 4, 2023",
-    description: "Rap legend",
-    artist: "Eminem",
-    title: "The Marshall Mathers LP",
-    coverUrl: "https://upload.wikimedia.org/wikipedia/en/a/ae/The_Marshall_Mathers_LP.jpg"
-  },
-  {
-    userName: "Rosi",
-    date: "November 5, 2023",
-    description: "Just love it!!",
-    artist: "Taylor Swift",
-    title: "1989",
-    coverUrl: "https://upload.wikimedia.org/wikipedia/en/f/f6/Taylor_Swift_-_1989.png"
-  }
-];
 
 export default Home;
