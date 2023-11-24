2eae7e51253d925cf35aa9034f18f6df5a363c8e
MichaelCarrr <98415558+MichaelCarrr@users.noreply.github.com>
2023-11-24 16:33:35 -0500

    Fixed routing on login, added sign out button

--- app/src/App.js
+++ app/src/App.js
@@ -1,39 +1,57 @@
-
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import About from "./pages/About.js";
 import Home from "./pages/Home.js";
 import Profile from "./pages/Profile.js";
 import LoginPage from "./pages/LoginPage.js";
-import Login from './components/auth/Login.jsx';
-import Register from './components/auth/Register.jsx';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const auth = getAuth();
+
+  useEffect(() => {
+    // Subscribe to authentication state changes
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setIsLoggedIn(!!user);
+    });
+
+    return () => unsubscribe(); // Cleanup subscription
+  }, [auth]);
+
   return (
     <Router>
       <nav>
         <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
+          {!isLoggedIn && (
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+          )}
           <li>
             <Link to="/about">About</Link>
           </li>
+          {isLoggedIn && (
           <li>
             <Link to="/profile">Profile</Link>
           </li>
-          <li>
-            <Link to="/Login">Login</Link>
-          </li>
+
+          )}
+      
+          {isLoggedIn && (
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+          )}
         </ul>
       </nav>
       <div className="container">
         <Switch>
-          <Route path="/" exact component={Home} />
+          <Route path="/" exact component={LoginPage} />
           <Route path="/about" exact component={About} />
           <Route path="/profile" exact component={Profile} />
-          <Route path="/login" exact component={LoginPage} />
-
+          <Route path="/home" exact component={Home} />
         </Switch>
       </div>
     </Router>
